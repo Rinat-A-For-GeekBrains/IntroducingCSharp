09152769c35c0c022de6b37e2539d9098d0a70b0
Rinat <alimbekovrl@gmail.com>
2023-02-14 23:12:48 +0500

    D:\Users\Rinat\OneDrive\Документы\GeekBrains\Code_Learning\HomeWorkCSharp\Seminar02-Tas k15\Program.cs(9,20): warning CS8600: Преобразование литерала, допускающего значение NU eDrive\Документы\GeekBrains\Code_Learning\HomeWorkCSharp\Seminar02-Task15\Seminar02-Tas k15.csproj]

---
+++ Seminar02-Task15/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int WeekDay)
+{
+    if (WeekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekDay(int number)
+{
+    if (number > 0 && number < 7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это не день недели");
+    return false;
+}
+int WeekDay = Prompt("Введите день недели ");
+if (ValidateWeekDay(WeekDay))
+{
+    if (IsWeekend(WeekDay))
+    {
+        System.Console.WriteLine("Выходной");
+    }
+    else
+        System.Console.WriteLine("Рабочий день");
+}

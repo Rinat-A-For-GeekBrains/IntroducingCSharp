47fa7c5bc8911f454c905790b155b18f8edd9693
Rinat <alimbekovrl@gmail.com>
2023-02-23 14:14:59 +0500

    Task about extent

---
+++ Seminar04-Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string inputFromLine = Console.ReadLine();
+    int result = Convert.ToInt32(inputFromLine);
+    return result;
+}
+
+int Stepen(int basis, int extent)
+{
+    int result = 1;
+       for (int i = 0; i < extent; i++)
+    {
+    result = result * basis;
+   
+    }
+    return result;
+}
+
+int basis = Prompt("Введите основание степени");
+int extent = Prompt("Введите степень");
+if (extent > 0){
+    Console.WriteLine($"результат={Stepen(basis, extent)}");
+}
+else 
+Console.WriteLine("Число степени не натуральное");

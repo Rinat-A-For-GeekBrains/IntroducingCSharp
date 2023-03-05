49dccea14239601fec5e853318f91d3a39ee7bdd
unknown <alimbekovrl@gmail.com>
2023-03-05 16:01:21 +0500

    Task Finished

---
+++ Seminar07-HomeTask52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMean(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            array[i] += matrix[j, i];
+
+
+        }
+        array[i] = array[i] / matrix.GetLength(0);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}, ");
+}
+int m = ReadInt("Введите количеcтво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+int[,] matrix = CreateMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine(matrix[0,1]);
+System.Console.WriteLine();
+PrintArray(ArithmeticMean(matrix));

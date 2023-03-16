4baa49ec6407a3027ea2c98886ed5d254c6f952f
unknown <alimbekovrl@gmail.com>
2023-03-16 19:58:16 +0500

    Task Accerman finished

---
+++ Seminar09-Hometask68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string prompt){
+Console.WriteLine(prompt);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = ReadInt("Введите число m: ");
+int n = ReadInt("Введите число n: ");
+
+int Accerman(int m, int n)
+{
+if (m == 0) return n+1;
+if (n == 0) return Accerman(m-1, 1);
+return Accerman(m-1,Accerman(m,n-1));
+}
+
+Console.Write("A(m,n) = " + Accerman(m,n));
